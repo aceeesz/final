8f6a4378a799983814438898a79865e0e797167a
aceeesz <qceefa1@tip.edu.ph>
2024-05-19 18:54:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ class_names = ['Rain', 'Shine', 'Cloudy', 'Sunrise']
 # Function to preprocess the input image
 def preprocess_image(image, target_size=(60, 40)):
     image = image.resize(target_size)
-    image = image.convert('RGB')  # Ensure the image is in RGB mode
     image = np.array(image)
     image = image / 255.0  # Normalize the image
     image = np.expand_dims(image, axis=0)  # Add batch dimension
@@ -29,26 +28,17 @@ st.title("Final Exam: Model Deployment in the Cloud")
 uploaded_file = st.file_uploader("Upload a weather image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    try:
-        # Check the type of the uploaded file
-        st.write(f"Uploaded file type: {uploaded_file.type}")
-        
-        # Read the image file
-        image = Image.open(uploaded_file)
-        
-        # Display the uploaded image
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    # Display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        # Make prediction
-        prediction = predict(image)
-        predicted_class_index = np.argmax(prediction, axis=1)[0]
-        predicted_class = class_names[predicted_class_index]
+    # Make prediction
+    prediction = predict(image)
+    predicted_class_index = np.argmax(prediction, axis=1)[0]
+    predicted_class = class_names[predicted_class_index]
 
-        # Display the prediction
-        st.success(f"Prediction: {predicted_class}")
-
-    except Exception as e:
-        st.error(f"Error occurred: {e}")
+    # Display the prediction
+    st.success(f"Prediction: {predicted_class}")
 
 # Instructions for the user
 st.markdown("""
